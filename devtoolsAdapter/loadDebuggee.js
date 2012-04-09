e46675c520db1673d676ac1f029c8b42480acaf6
johnjbarton <johnjbarton@johnjbarton.com>
2012-04-09 11:31:57 -0700

    patch devtools with script tag rather than same-domain injection. Unwrap iframe, call appEnd directly from devtools.html window

--- devtoolsAdapter/loadDebuggee.js
+++ devtoolsAdapter/loadDebuggee.js
@@ -2,14 +2,11 @@
 
 var SiriusBase = 'chrome-extension://fkhgelnmojgnpahkeemhnbjndeeocehc';
 
-// TODO change crx2app to use ../..
 window.crx2appBase = SiriusBase + '/crx2app/extension'; // for workaround 
 
 require({
     paths: {
-      'crx2app': SiriusBase + '/crx2app/extension',
-      'atopwi': SiriusBase + '/atopwi',
-      'RESTChannel': SiriusBase + '/RESTChannel'
+      'crx2app': '../../../crx2app/extension',
     }
 });
 
