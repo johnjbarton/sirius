86498a60779d5629c082def03346e17089afcf38
johnjbarton <johnjbarton@johnjbarton.com>
2012-05-22 20:11:28 -0700

    remove atopwi fake submodule

--- extension/atopwi/test/extendDevTools.js
+++
@@ -1,9 +0,0 @@
-// This code is loaded by 'options' settings in Sirius
-// It loads into devtools.html as a devtools extension.
-
-chrome.devtools.panels.create("Test", '', 'atopwi/test/testPanel.html', function(PanelAPI) {
-  console.log('extendDevTools.html PANEL callback', arguments);
-  PanelAPI.onShown.addListener(function(win) {
-    console.log('extendDevTools.html panel.onShown callback', arguments);
-  });
-});
