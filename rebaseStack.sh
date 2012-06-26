aac9303279ed499c502cba55fd897c67d0713642
johnjbarton <johnjbarton@johnjbarton.com>
2012-06-26 11:08:46 -0700

    shell script to rebase patch stack

---
+++ rebaseStack.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+set -e
+git rebase syncToWebKit iframeable
+git rebase iframeable remoteDebug
+git rebase remoteDebug DebuggerProtocol
+git rebase DebuggerProtocol extendable
+git rebase extendable master
