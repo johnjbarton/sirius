3e932c0fb4dd055bc0646127e5ad3d6216e44748
johnjbarton <johnjbarton@johnjbarton.com>
2012-06-28 14:04:27 -0700

    add new patch branch purple for extension/Purple

--- rebaseStack.sh
+++ rebaseStack.sh
@@ -4,6 +4,7 @@ git rebase syncToWebKit iframeable
 git rebase iframeable remoteDebug
 git rebase remoteDebug DebuggerProtocol
 git rebase DebuggerProtocol extendable
-git rebase extendable patchStackTop
+git rebase extendable purple
+git rebasepurple patchStackTop
 git checkout master
 git merge patchStackTop
