27bfe95a432c045eb93cd93c8fe763a5bc63835a
johnjbarton <johnjbarton@johnjbarton.com>
2012-06-27 17:04:14 -0700

    add patchStackTop and merge into master

--- rebaseStack.sh
+++ rebaseStack.sh
@@ -4,4 +4,6 @@ git rebase syncToWebKit iframeable
 git rebase iframeable remoteDebug
 git rebase remoteDebug DebuggerProtocol
 git rebase DebuggerProtocol extendable
-git rebase extendable master
+git rebase extendable patchStackTop
+git checkout master
+git merge patchStackTop
