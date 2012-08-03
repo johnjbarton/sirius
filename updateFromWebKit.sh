7bcf188d87b70f17fac610899ab12a227eada91f
johnjbarton <johnjbarton@johnjbarton.com>
2012-08-03 12:35:34 -0700

    patch the devtools.html during update from WebKit

--- updateFromWebKit.sh
+++ updateFromWebKit.sh
@@ -9,3 +9,5 @@ echo $BUILD
 cp $BUILD/devtools_extension_api.js extension/atopwi/inspector/front-end
 cp $BUILD/InspectorBackendCommands.js extension/atopwi/inspector/front-end
 cp $BUILD/devtools.html extension/atopwi/inspector/front-end
+
+patch -p1 < extension/atopwi/devtoolsAdapter/devtools.html.patch
