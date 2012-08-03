25d3bb2f27e67882fcb33436fb4939c81e062a78
johnjbarton <johnjbarton@johnjbarton.com>
2012-08-03 12:31:50 -0700

    copy built files with shell script updateFromWebKit.sh

---
+++ updateFromWebKit.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# update atopwi from WebKit
+set -xev
+git checkout syncToWebKit
+
+BUILD=$( cat .git/tx-other-repos/inspector | sed -e 's|/third_party/WebKit/Source/WebCore/inspector/front-end/|/out/Release/resources/inspector|' )
+echo $BUILD
+
+cp $BUILD/devtools_extension_api.js extension/atopwi/inspector/front-end
+cp $BUILD/InspectorBackendCommands.js extension/atopwi/inspector/front-end
+cp $BUILD/devtools.html extension/atopwi/inspector/front-end
