d4fb7d773f5e24f6a213d856b1cba2d1f6d9a711
johnjbarton <johnjbarton@johnjbarton.com>
2012-09-12 11:37:45 -0700

    add --merge option and write on built-devtools

--- updateFromWebKit.sh
+++ updateFromWebKit.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # update atopwi from WebKit
 set -xev
-git checkout syncToWebKit
+git checkout built-devtools
 
 BUILD=$( cat .git/tx-other-repos/inspector | sed -e 's|/third_party/WebKit/Source/WebCore/inspector/front-end/|/out/Release/resources/inspector|' )
 echo $BUILD
@@ -10,4 +10,4 @@ cp $BUILD/devtools_extension_api.js extension/atopwi/inspector/front-end
 cp $BUILD/InspectorBackendCommands.js extension/atopwi/inspector/front-end
 cp $BUILD/devtools.html extension/atopwi/inspector/front-end
 
-patch -p1 < extension/atopwi/devtoolsAdapter/devtools.html.patch
+patch --merge -p1 < extension/atopwi/devtoolsAdapter/devtools.html.patch
